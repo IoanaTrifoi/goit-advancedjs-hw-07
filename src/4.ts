23e82ae328f60a85105c2b77b93557fd2eafd3e1
IoanaTrifoi <ioanatrifoi610@gmail.com>
2026-03-30 23:46:39 +0200

    Add files via upload

---
+++ src/4.ts
@@ -0,0 +1,54 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person entered the house.');
+    } else {
+      console.log('Door is closed. Cannot enter.');
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('The door is now open.');
+    } else {
+      console.log('Wrong key! The door remains closed.');
+    }
+  }
+}
+
+// Test
+const myKey = new Key();
+const myHouse = new MyHouse(myKey);
+const me = new Person(myKey);
+
+myHouse.openDoor(me.getKey());
+myHouse.comeIn(me);
+
+export {};
